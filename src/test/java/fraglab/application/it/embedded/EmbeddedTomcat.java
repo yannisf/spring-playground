d6a32fc016837fe73f852c5cc62a9749c1ce203f
yannisf <yannisf@ouranos>
2018-08-17 17:22:54 +0300

    When using embedded tomcat with an already provided classpath (e.g. through maven testing) there is no need to load the classes, and actually can create classloading conflicts

--- src/test/java/fraglab/application/it/embedded/EmbeddedTomcat.java
+++ src/test/java/fraglab/application/it/embedded/EmbeddedTomcat.java
@@ -12,24 +12,15 @@ public class EmbeddedTomcat implements EmbeddedServer {
 
     private Tomcat tomcat;
     private static final int PORT = 8080;
-    private static final String CONTEXT_PATH = "/";
+    private static final String CONTEXT_PATH = "";
     private static final String WEBAPP_DIR = "src/main/webapp/";
-    private static final String CLASSES_DIR = "target/classes";
 
     @Override
     public void start() throws Exception {
         tomcat = new Tomcat();
         tomcat.setPort(PORT);
-
         String webAppDirPath = new File(WEBAPP_DIR).getAbsolutePath();
-        StandardContext context = (StandardContext) tomcat.addWebapp(CONTEXT_PATH, webAppDirPath);
-
-        String webInfClassesPath = new File(CLASSES_DIR).getAbsolutePath();
-        WebResourceRoot resources = new StandardRoot(context);
-        DirResourceSet resourceSet = new DirResourceSet(resources, "/WEB-INF/classes", webInfClassesPath, "/");
-        resources.addPreResources(resourceSet);
-        context.setResources(resources);
-
+        tomcat.addWebapp(CONTEXT_PATH, webAppDirPath);
         tomcat.start();
     }
 
