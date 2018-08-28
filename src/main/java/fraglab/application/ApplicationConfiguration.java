c7d8a959cce30f942e53bb43ed1e9b6f1c6609f2
yannisf <yannisf@ouranos>
2018-08-28 09:30:08 +0300

    - Added swagger - Externalized configuration file

--- src/main/java/fraglab/application/ApplicationConfiguration.java
+++ src/main/java/fraglab/application/ApplicationConfiguration.java
@@ -1,8 +1,7 @@
 package fraglab.application;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -10,24 +9,42 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @Configuration
 @EnableWebMvc
+@EnableAspectJAutoProxy
+@EnableSwagger2
 @EnableTransactionManagement
 @ComponentScan(basePackages = {"fraglab"})
 @EnableJpaRepositories(basePackages = "fraglab")
-public class ApplicationConfiguration {
+@PropertySource("classpath:/jdbc.properties")
+public class ApplicationConfiguration implements WebMvcConfigurer {
+
+    @Value("${jdbc.driver}")
+    private String driverClassName;
+
+    @Value("${jdbc.url}")
+    private String url;
+
+    @Value("${jdbc.username}")
+    private String username;
+
+    @Value("${jdbc.password}")
+    private String password;
 
     @Bean
     public DriverManagerDataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
-        dataSource.setUrl("jdbc:hsqldb:hsql://localhost/xdb");
-        dataSource.setUsername("SA");
-        dataSource.setPassword("");
+        dataSource.setDriverClassName(driverClassName);
+//        dataSource.setUrl("jdbc:hsqldb:file:/tmp/xyzuvw");
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
         return dataSource;
     }
 
